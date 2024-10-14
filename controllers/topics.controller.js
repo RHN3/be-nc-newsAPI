bc3a46d9efb31924aaff749fd135a379e436a534
RHN3 <rhn.0033@gmail.com>
2024-10-14 15:32:43 +0100

    completed task 3

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,4 +1,10 @@
 const { selectTopics } = require("../models/topics.model");
+const endpoints = require("../endpoints.json");
+
+exports.getEndpoints = (req, res, next) => {
+  console.log(endpoints);
+  return res.status(200).send(endpoints);
+};
 
 exports.getTopics = (req, res, next) => {
   return selectTopics()
